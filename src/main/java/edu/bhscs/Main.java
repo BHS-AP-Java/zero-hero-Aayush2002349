d700a2954b47f0c1c17f149ab62cc726fa5f2656
Aayush2002349 <aayush15genius@gmail.com>
2025-09-15 17:01:29 +0000

    switched prints for printlns

--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -24,6 +24,8 @@ class Main {
     System.out.println(A + "?" + A + num);
 
     //Why is A + A + num 296 (maybe a as an int in bits is (296-102)/2 = 97 which is 01100001, 0110 may represent that this item is a char and then 1 may be the first letter (but i should expect that 0000 represents the first number))
+
+    //I switched out the prints for println's so i could differentiate each message more easily
     System.out.println(A + A + num);
 
     System.out.println();
