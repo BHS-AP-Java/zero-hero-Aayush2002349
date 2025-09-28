ab462320ff54b7ac68895c83786d0fd491cd7108
Aayush2002349 <aayush15genius@gmail.com>
2025-09-28 01:55:39 +0000

    Finished delivery, started creating orders

--- src/main/java/edu/bhscs/Bakery2.java
+++ src/main/java/edu/bhscs/Bakery2.java
@@ -2,9 +2,9 @@ package edu.bhscs;
 
 public class Bakery2 {
 
-  //Fields
+  // Fields
 
-  //Kitchen related stuff
+  // Kitchen related stuff
   int[][] layout;
   Cake[][] cakeLocations;
   Person[][] chefLocations;
@@ -15,22 +15,21 @@ public class Bakery2 {
   int width;
   int height;
 
-  //Customer related stuff
+  // Customer related stuff
   String name;
 
   Cake[] storedCakes;
 
-
-  //Layout:
-  //0 = empty
-  //1 = chef starting location(considered empty)
-  //2 = cake mix
-  //3 = oven
-  //4 = counter
-  //5 = cutting station
-  //6 = delivery station
-  //7 = trash
-  public Bakery2(int[][] layout, int width, int height, int maxCakeStorage, String name){
+  // Layout:
+  // 0 = empty
+  // 1 = chef starting location(considered empty)
+  // 2 = cake mix
+  // 3 = oven
+  // 4 = counter
+  // 5 = cutting station
+  // 6 = delivery station
+  // 7 = trash
+  public Bakery2(int[][] layout, int width, int height, int maxCakeStorage, String name) {
     this.name = name;
     this.width = width;
     this.height = height;
@@ -40,102 +39,105 @@ public class Bakery2 {
     this.storedCakes = new Cake[maxCakeStorage];
 
     this.cakeCookingTimers = new int[height][width];
-    for(int y = 0; y < this.height; y++){
+    for (int y = 0; y < this.height; y++) {
       for (int x = 0; x < this.width; x++) {
         this.cakeCookingTimers[y][x] = 0;
       }
     }
   }
 
-  //Boolean returns if the chef was hired or not
-  public Boolean hireChef(Person chef){
-    for(int y = 0; y < this.height; y++){
+  // Boolean returns if the chef was hired or not
+  public Boolean hireChef(Person chef) {
+    for (int y = 0; y < this.height; y++) {
       for (int x = 0; x < this.width; x++) {
 
-        if(this.layout[y][x] == 1) {
+        if (this.layout[y][x] == 1) {
           if (this.chefLocations[y][x] == null) {
             this.chefLocations[y][x] = chef;
             return true;
           }
         }
-
       }
     }
     return false;
   }
 
-  //Boolean returns whether the cake was or was not placed
-  public Boolean placeCake(Cake cake,int x,int y){
+  // Boolean returns whether the cake was or was not placed
+  public Boolean placeCake(Cake cake, int x, int y) {
 
-    //Place cake just places the cake (makes sure there isn't already a cake there and that a cake can be placed at the given location)
-    //All behavior of what to do when the cake is placed happens during "tick"
-    if(this.cakeLocations[y][x] != null){
+    // Place cake just places the cake (makes sure there isn't already a cake there and that a cake
+    // can be placed at the given location)
+    // All behavior of what to do when the cake is placed happens during "tick"
+    if (this.cakeLocations[y][x] != null) {
       return false;
     }
 
-    if(this.layout[y][x] >= 3){
+    if (this.layout[y][x] >= 3) {
       this.cakeLocations[y][x] = cake;
       return true;
     }
     return false;
   }
 
-  //Will return null if no cake is there
-  public Cake pickUpCake(int x, int y){
+  // Will return null if no cake is there
+  public Cake pickUpCake(int x, int y) {
     Cake cakeTemp = this.cakeLocations[y][x];
     this.cakeLocations[y][x] = null;
     this.cakeCookingTimers[y][x] = 0;
     return cakeTemp;
-
   }
 
-  //Cuts the cake at the given location
-  public void cutCake(int x, int y){
+  // Cuts the cake at the given location
+  public void cutCake(int x, int y) {
     this.cakeLocations[y][x].cut(6);
   }
 
-  //Looks at all placed cakes and figures out what to do with them
-  public void tick(){
+  // Looks at all placed cakes and figures out what to do with them
+  public void tick() {
 
-
-    for(int y = 0; y < this.height; y++){
+    for (int y = 0; y < this.height; y++) {
       for (int x = 0; x < this.width; x++) {
 
-        //Makes sure there is a cake there
-        if(this.cakeLocations[y][x] != null){
+        // Makes sure there is a cake there
+        if (this.cakeLocations[y][x] != null) {
 
           // 3 = oven
           // 6 = delivery station
           // 7 = trash
-          if(this.layout[y][x] == 3){
-            if(this.cakeCookingTimers[y][x] == this.cookingTime){
+          if (this.layout[y][x] == 3) {
+            if (this.cakeCookingTimers[y][x] == this.cookingTime) {
               this.cakeCookingTimers[y][x] = 0;
               this.cakeLocations[y][x].bake();
             }
             this.cakeCookingTimers[y][x] += 1;
           }
-          if(this.layout[y][x] == 6){
+          if (this.layout[y][x] == 6) {
             this.deliver(x, y);
           }
-          if(this.layout[y][x] == 7){
-            //Picking up the cake without giving it to anyone is the equivalent of deleting it
+          if (this.layout[y][x] == 7) {
+            // Picking up the cake without giving it to anyone is the equivalent of deleting it
             this.pickUpCake(x, y);
           }
         }
-
       }
     }
   }
 
-  public Cake deliver(int x, int y) {
-    return this.pickUpCake(x, y);
+  //The boolean returns whether or not the cake was delivered
+  public Boolean deliver(int x, int y) {
+    Cake cake = this.pickUpCake(x,y);
+    if(cake.isEdible){
+      this.storeCake(cake);
+      return true;
+    }
+    return false;
   }
 
-  //The boolean returns whether or not the cake was stored
-  public Boolean storeCake(Cake cake){
+  // The boolean returns whether or not the cake was stored
+  public Boolean storeCake(Cake cake) {
 
-    for(int i = 0; i < this.storedCakes.length; i++){
-      if(this.storedCakes[i] == null){
+    for (int i = 0; i < this.storedCakes.length; i++) {
+      if (this.storedCakes[i] == null) {
         this.storedCakes[i] = cake;
         return true;
       }
@@ -144,10 +146,10 @@ public class Bakery2 {
     return false;
   }
 
-  //Returns null when there is no cake of the given type
-  public Cake getStoredCake(String type){
-    for(int i = 0; i < this.storedCakes.length; i++){
-      if(this.storedCakes[i].type == type){
+  // Returns null when there is no cake of the given type
+  public Cake getStoredCake(String type) {
+    for (int i = 0; i < this.storedCakes.length; i++) {
+      if (this.storedCakes[i].type == type) {
         return this.storedCakes[i];
       }
     }
--- src/main/java/edu/bhscs/Game.java
+++ src/main/java/edu/bhscs/Game.java
@@ -3,11 +3,29 @@ package edu.bhscs;
 public class Game {
 
   Bakery2 bakery;
+  String[] orders = new String[6];
+  int completedOrders = 0;
 
-  public Game(int[][] layout){
+  public Game(int[][] layout) {
 
-    this.bakery = new Bakery2(layout,layout[0].length,layout.length,10,"The Bakery");
-
+    this.bakery = new Bakery2(layout, layout[0].length, layout.length, 10, "The Bakery");
   }
 
+  public void createOrder(String type) {
+    for(int i = 0; i < this.orders.length; i++){
+      if(this.orders[i] == null){
+        this.orders[i] = type;
+        return;
+      }
+    }
+  }
+
+  public void completeOrders(){
+    for(int i = 0; i < this.orders.length; i++){
+      Cake cake = this.bakery.getStoredCake(this.orders[i]);
+      if(cake != null){
+        
+      }
+    }
+  }
 }
